fbca3f741d153994dac7368e7b48afd9bf25ffc5
TroyL <troy.lochner@gmail.com>
2020-10-15 10:17:23 -0400

    restructured app:

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
+// ===========================================================
 // Dependencies
 // ===========================================================
 var express = require("express");
 var uuid = require("uuid");
-var app = express();
 var path = require("path");
 const fs = require("fs");
 const util = require("util");
-
 var dt = require('./modules/script');
 
 
@@ -15,20 +14,35 @@ var dayjs = require('dayjs')
 //import dayjs from 'dayjs' // ES 2015
 dayjs().format()
 
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+// Tells node that we are creating an "express" server
+var app = express();
 
-var PORT = 3000;
-
-
-//SETUP WRITE FILE OPTIONS
-const writeFileAsync = util.promisify(fs.writeFile);
-
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// ================================================================================
+//SETUP WRITE FILE OPTIONS
+const writeFileAsync = util.promisify(fs.writeFile);
+// ================================================================================
+
+// ================================================================================
 //GET UNREAD REQUESTS
 var unreadRequest = [] ; 
 
@@ -38,47 +52,14 @@ fs.readFile("unreadRequest.json", "utf8", function(error, data) {
     }
     unreadRequest= JSON.parse(data);
   });
+  // ================================================================================
 
 
-
-//ROUTES
-// =============================================================
-// Basic route that sends the user first to the AJAX Page
-
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "views/index.html"));
-});
-
-app.get("/submit", function (req, res) {
-    res.sendFile(path.join(__dirname, "views/submit.html"));
-});
-
-app.get("/api/request/:uuid", function(req,res){
-    var uuid = req.params.uuid ;
-     console.log(uuid)
-    let obj = unreadRequest.find(obj => obj.uuid == uuid);
-    res.json(obj)
-});
-
-app.post("/api/submit", function (req, res) {
-    var newRequest = req.body;
-
-    newRequest.uuid = uuid.v4()
-    newRequest.timestamp = dayjs().format();
-    unreadRequest.push(newRequest);
-
-    res.json(newRequest);
-    //console.log(newRequest);
-
-    writeFileAsync("unreadRequest.json", JSON.stringify(unreadRequest))
-});
-
-app.get("/api/requests/", function(req,res){
-    return res.json(unreadRequest);
-})
-
-// Listener
-// ===========================================================
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
-});
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
